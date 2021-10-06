a8acebf038606f7845d9698179370a53d2ec5545
Rohit ranjan <ranjanrohit812@gmail.com>
2021-10-06 10:41:20 +0530

    Search Box Added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:zomato_ui/screens/go_out_page.dart';
 import 'package:zomato_ui/screens/order_page.dart';
 import 'package:zomato_ui/screens/pro_page.dart';
+import 'package:zomato_ui/widgets/search_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -30,29 +31,42 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          title: Row(
-            children: const <Widget>[
-              Icon(Icons.location_pin, color: Colors.red),
-              Text(
-                ' Vayusena Nagar',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
+        appBar: _selectedIndex != 2
+            ? AppBar(
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                title: Row(
+                  children: const <Widget>[
+                    Icon(Icons.location_pin, color: Colors.red),
+                    Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Vayusena Nagar',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.transparent,
+                          shadows: [
+                            Shadow(
+                                offset: Offset(0, -3.5), color: Colors.black),
+                          ],
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                          decorationStyle: TextDecorationStyle.dashed,
+                          decorationColor: Colors.black54,
+                          decorationThickness: 2,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
+                actions: <Widget>[
+                  IconButton(
+                    icon: const Icon(Icons.menu, color: Colors.black),
+                    onPressed: () {},
+                  )
+                ],
               )
-            ],
-          ),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.menu, color: Colors.black),
-              onPressed: () {},
-            )
-          ],
-        ),
+            : null,
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: _selectedIndex,
           onTap: _onItemTapped,
@@ -73,8 +87,13 @@ class _HomePageState extends State<HomePage> {
             )
           ],
         ),
-        body: Center(
-          child: _widgetOptions.elementAt(_selectedIndex),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              child: SearchBox('Restaurant name, cusine, or a dish'),
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SearchBox extends StatefulWidget {
+  String hintText;
+  SearchBox(this.hintText, {Key? key}) : super(key: key);
+
+  @override
+  State<SearchBox> createState() => _SearchBoxState();
+}
+
+class _SearchBoxState extends State<SearchBox> {
+  TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3,
+      shape: RoundedRectangleBorder(
+        side: const BorderSide(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Row(
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Icon(
+              Icons.search,
+              color: Colors.red,
+            ),
+          ),
+          Expanded(
+            child: TextField(
+              controller: controller,
+              onEditingComplete: () {
+                FocusScopeNode currentFocus = FocusScope.of(context);
+
+                if (!currentFocus.hasPrimaryFocus) {
+                  currentFocus.unfocus();
+                }
+              },
+              decoration: InputDecoration(
+                hintText: widget.hintText,
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
