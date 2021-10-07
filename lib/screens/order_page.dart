85e58ff846f070f54e0e67329d39bbf51e4e9b00
Rohit ranjan <ranjanrohit812@gmail.com>
2021-10-07 11:47:28 +0530

    Added Restaurant Bar

--- lib/screens/order_page.dart
+++ lib/screens/order_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:zomato_ui/widgets/food_bar.dart';
 import 'package:zomato_ui/widgets/horizontal_card_list.dart';
+import 'package:zomato_ui/widgets/restaurant_bar.dart';
 import 'package:zomato_ui/widgets/search_bar.dart';
 
 class OrderPage extends StatelessWidget {
@@ -33,7 +34,19 @@ class OrderPage extends StatelessWidget {
                 child: Image(image: AssetImage('assets/images/offer.png')),
               ),
               Padding(
-                padding: EdgeInsets.only(left: 15.0),
+                padding: EdgeInsets.only(left: 15.0, bottom: 5.0),
+                child: Text(
+                  'Top brands for you',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              RestaurantBar(),
+              RestaurantBar(rowNo: 2),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0, bottom: 5.0),
                 child: Text(
                   'Eat what makes you happy',
                   style: TextStyle(
---
+++ lib/widgets/restaurant_bar.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+
+class RestaurantBar extends StatelessWidget {
+  const RestaurantBar({
+    Key? key,
+    this.rowNo = 1,
+  }) : super(key: key);
+
+  final int rowNo;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          rowNo == 1
+              ? const RestaurantBarItem(
+                  imageUrl:
+                      'https://www.haldirams.com/media/wysiwyg/Contact_Us_Banner.jpg',
+                  title: 'Haldiram',
+                )
+              : const RestaurantBarItem(
+                  imageUrl:
+                      'https://scontent.fnag1-3.fna.fbcdn.net/v/t1.6435-9/97945497_1120509294994364_8440036943424651264_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=973b4a&_nc_ohc=b9v8gmOmO8UAX9h90qy&_nc_ht=scontent.fnag1-3.fna&oh=2d9a9b7fc6a3306487b70dded977571d&oe=61840620',
+                  title: 'Behrouz',
+                ),
+          rowNo == 1
+              ? const RestaurantBarItem(
+                  imageUrl:
+                      'https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/KFC_logo.svg/220px-KFC_logo.png',
+                  title: 'KFC',
+                )
+              : const RestaurantBarItem(
+                  imageUrl:
+                      'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Dominos_pizza_logo.svg/512px-Dominos_pizza_logo.png',
+                  title: 'Domino\'s',
+                ),
+          rowNo == 1
+              ? const RestaurantBarItem(
+                  imageUrl:
+                      'https://scontent.fnag1-2.fna.fbcdn.net/v/t1.6435-9/89203401_154565692674421_6996872861897981952_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=973b4a&_nc_ohc=E2_GzS48ivAAX-uVr9U&_nc_oc=AQnVtujmHHT7kL7TSwsqZzEmwlGtppSJKZci2Wj9dlyyg5tdH4A-tc9rtYbRrF8Vaj01WwW_Epx5wQH3K6_ivHVp&_nc_ht=scontent.fnag1-2.fna&oh=584eee16fcb3049328fb527a99dc4d3c&oe=61835348',
+                  title: 'BombayWala',
+                )
+              : const RestaurantBarItem(
+                  imageUrl:
+                      'https://1000logos.net/wp-content/uploads/2017/05/Pizza-Hut-Logo-1999-500x281.jpg',
+                  title: 'Pizza Hut',
+                ),
+          rowNo == 1
+              ? const RestaurantBarItem(
+                  imageUrl:
+                      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSs7PEkty85e0cQbXmp0G_7xBkpH85MVPno0Q&usqp=CAU',
+                  title: 'Subway',
+                )
+              : const RestaurantBarItem(
+                  imageUrl:
+                      'https://logos-download.com/wp-content/uploads/2016/03/McDonalds_Logo_2018-700x700.png',
+                  title: 'MacDonald\'s',
+                ),
+        ],
+      ),
+    );
+  }
+}
+
+class RestaurantBarItem extends StatelessWidget {
+  const RestaurantBarItem(
+      {Key? key, required this.imageUrl, required this.title})
+      : super(key: key);
+
+  final String imageUrl;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ClipRRect(
+            borderRadius: BorderRadius.circular(50.0),
+            child: Image.network(
+              imageUrl,
+              height: 80,
+              width: 80,
+              fit: BoxFit.cover,
+            )),
+        const SizedBox(
+          height: 5.0,
+        ),
+        Text(title)
+      ],
+    );
+  }
+}
